64a674a49e70963b6be2c3114e42f4bde2adebd6
USAMI Kenta <tadsan@pixiv.com>
2015-12-02 00:48:54 +0900

    Remove autoloader

--- lib/autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-spl_autoload_register(function($className) {
-    if (strpos($className, 'Phactory\\') === 0) {
-        require_once __DIR__ . '/' . str_replace('\\', '/', $className) . '.php';
-    }
-});
